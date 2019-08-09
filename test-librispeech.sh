0add5c89c926756d86b10114f3ca2f9a5963ed86
Florian Thonig <florian.thonig96@gmail.com>
2019-08-09 20:15:42 +0200

    add sample for testing and adjusted testing script

--- test-librispeech.sh
+++ test-librispeech.sh
@@ -6,18 +6,18 @@ INPUT_FILE="$1"
 if [ ! -f "$INPUT_FILE" ]; then
 	echo "Unable to open $INPUT_FILE"
 	echo "You need to provide an input file"
-	echo "Usage example:\n./test-librispeech sample.mp3"
+	echo "Usage example:\n./test-librispeech sample.flac"
 	exit 1
 fi
 
 TFILE=$(mktemp ./XXXXXX.tfrecord)
-SFILE=$(mktemp ./resultXXXXX.txt)
+SFILE=$(mktemp ./result-XXXXX.txt)
 
 echo "Preprocess $INPUT_FILE"
-python3.6 testing/preprocess.py --input $INPUT_FILE  --output_file $TFILE
+python3 testing/preprocess.py --input $INPUT_FILE  --output_file $TFILE
 
 echo "Do inference"
-python3.6 infer.py --data $TFILE --vocab $VOCAB_PATH --model_dir $MODEL_DIR --save $SFILE --beam_width 10 --batch_size 1
+python3 infer.py --data $TFILE --vocab $VOCAB_PATH --model_dir $MODEL_DIR --save $SFILE --beam_width 50 --batch_size 1
 
 rm -f "$TFILE"
 
